18a6712489914d6581b56a0effd5be5db84f89fe
Daniel Duncan <dduncan@medallia.com>
2018-04-05 17:17:08 -0700

    Completes lecture 125, adds permanent modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+
+    const ingredientsSummary =
+        Object.entries(props.ingredients)
+            .map(entry =>
+                <li key={entry[0]}>
+                    <span style={{textTransform: 'capitalize'}}>{entry[0]}</span>: {entry[1]}
+                </li>
+            );
+
+    return (
+        <Aux>
+            <h3>Your Order:</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    );
+};
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildsControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const BASE_BURGER_PRICE = 4;
 
@@ -73,6 +75,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     addIngredientHandler={this.addIngredientHandler}
