81316893a68b05c59d850628a233c5d24cb721c4
Daniel Duncan <dduncan@medallia.com>
2018-04-08 07:00:20 -0700

    Completes lecture 139, changes menu to hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import classes from './DrawerToggle.css';
+
 const drawerToggle = (props) => {
     return (
-        <div onClick={props.onClick}>Menu</div>
+        <div className={classes.DrawerToggle} onClick={props.onClick}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 };
 
