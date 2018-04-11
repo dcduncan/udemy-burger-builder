1eec2782abd289c13cce91b915f6ba26e7b4230e
Daniel Duncan <dduncan@medallia.com>
2018-04-10 19:44:11 -0700

    Completes lecture 168, posts burger to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildsControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const BASE_BURGER_PRICE = 4;
 
 const INGREDIENT_PRICES = {
@@ -77,7 +79,26 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert('Purchased');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'DJ',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '30584',
+                    country: 'Poland'
+                },
+                email: 'dcduncan.code@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+
+        axios.post('/orders.json', order)
+            .then((response) => console.log(response))
+            .catch((error) => console.log(error));
+
+
     };
 
     render() {
