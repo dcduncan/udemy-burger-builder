2806a85781cc381fa8ea5fc56fbbae163728ae32
Daniel Duncan <dduncan@medallia.com>
2018-04-10 19:36:57 -0700

    Completes lecture 167, creates axios instance

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// https://burger-builder-f51ac.firebaseio.com/
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
