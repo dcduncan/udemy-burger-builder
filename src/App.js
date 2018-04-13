bde6c064aad24f4a1b7429c22119137b7b09e057
Daniel Duncan <dduncan@medallia.com>
2018-04-13 13:34:47 -0700

    Completes lecture 209, hooks up checkout page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+              <Route path="/checkout" component={Checkout}/>
+              <Route component={BurgerBuilder}/>
+          </Switch>
       </Layout>
     );
   }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,31 +78,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-
-        this.setState({loading: true});
-
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'DJ',
-                address: {
-                    street: 'test street 1',
-                    zipCode: '30584',
-                    country: 'Poland'
-                },
-                email: 'dcduncan.code@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
-
-        axios.post('/orders.json', order)
-            .then((response) => console.log(response))
-            .catch((error) => console.log(error))
-            .finally(() => this.setState({
-                loading: false,
-                purchasing: false
-            }));
+        this.props.history.push('/checkout')
     };
 
     getOrderSummaryOrLoading = () => {
