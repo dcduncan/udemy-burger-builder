a5f487d916b25256f41cba6864ade114453e42bf
Daniel Duncan <dduncan@medallia.com>
2018-04-15 08:03:34 -0700

    Completes lecture 215, adds order page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <Layout>
           <Switch>
+              <Route path="/orders" component={Orders}/>
               <Route path="/checkout" component={Checkout}/>
               <Route component={BurgerBuilder}/>
           </Switch>
